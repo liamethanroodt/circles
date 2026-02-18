4e7f857e3b47b9af76fd714c6ef1d57fabc98f83
Liam Roodt <liam@mobii.com>
2026-02-18 15:03:20 +0200

    Added sql server db

--- circles.AppHost/AppHost.cs
+++ circles.AppHost/AppHost.cs
@@ -2,9 +2,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
+var sql = builder.AddSqlServer("sql")
+    .AddDatabase("circlesdb");
+
 var server = builder.AddProject<Projects.circles_Server>("server")
     .WithReference(cache)
+    .WithReference(sql)
     .WaitFor(cache)
+    .WaitFor(sql)
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
