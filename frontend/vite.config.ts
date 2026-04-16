6c4b8e3f96e337cb6bc9b6fb3ffe559b5a9dd17e
Liam Roodt <liam@mobii.com>
2026-04-16 20:33:50 +0200

    Installed shadcn, some components, and did basic replacement on AuthPage.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,25 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ quoteStyle: 'single' }), react()],
-  server: {
-    proxy: {
-      // Proxy API calls to the app service
-      '/api': {
-        target: process.env.SERVER_HTTP || process.env.SERVER_HTTPS,
-        changeOrigin: true,
-        secure: false
-      }
+    plugins: [TanStackRouterVite({ quoteStyle: 'single' }), react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    server: {
+        proxy: {
+            // Proxy API calls to the app service
+            '/api': {
+                target: process.env.SERVER_HTTP || process.env.SERVER_HTTPS,
+                changeOrigin: true,
+                secure: false
+            }
+        }
     }
-  }
 })
