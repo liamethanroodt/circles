39d72c14312b1130c9c054f268aa07e5544a6da9
Liam Roodt <liam@mobii.com>
2026-04-29 21:20:11 +0200

    Formatted some files.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,24 +6,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ quoteStyle: "single" }),
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      // Proxy API calls to the app service
-      "/api": {
-        target: process.env.SERVER_HTTP || process.env.SERVER_HTTPS,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+	plugins: [TanStackRouterVite({ quoteStyle: "single" }), react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		proxy: {
+			// Proxy API calls to the app service
+			"/api": {
+				target: process.env.SERVER_HTTP || process.env.SERVER_HTTPS,
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
 });
